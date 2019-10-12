68d42c4ac3709103bb9cfdbd4275eb9d58f2ab5b
Tomi_mac <pintertamas99@gmail.com>
2019-10-12 16:06:56 +0200

    index, carList, soldCars signUp browseCars inspectCar pages are ready signUp is somehow fucked up, needs some fixing in the future

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Load routing
+require('./route/index')(app);
+
 app.use(express.static('static'));
 app.listen(3000, function () {
     console.log('Hello :3000');
